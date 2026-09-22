f7b5fc922686c55e5c6759e1fdf3c3f963f1c1a5
miguelcarvalho-prog <miguel.carvalho@ba.estudante.senai.br>
2026-09-22 20:06:12 +0000

    cria estrutura do backend

--- backend/server.js
+++ backend/server.js
@@ -13,21 +13,24 @@ app.get("/", (req, res) => {
             nome: "Notebook",
             preco: 3500,
             estoque: 10,
-            categoria: "Eletrônicos"
+            categoria: "Eletrônicos",
+            imagem: "https://cdn1.smartprix.com/rx-in7jdvIGp-w1200-h1200/n7jdvIGp.jpg"
         },
         {
             id: 2,
             nome: "Celular",
             preco: 1800,
             estoque: 15,
-            categoria: "Eletrônicos"
+            categoria: "Eletrônicos",
+            imagem: "https://tse4.mm.bing.net/th/id/OIP.IPHp8t692Sz_u5tZeOfWiQHaHa?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
         },
         {
             id: 3,
             nome: "Mochila",
             preco: 120,
             estoque: 20,
-            categoria: "Acessórios"
+            categoria: "Acessórios",
+            imagem: "https://tse3.mm.bing.net/th/id/OIP.Zx2W3x-db9GIzuCLC8bsQQHaHa?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
         }
     ];
 
