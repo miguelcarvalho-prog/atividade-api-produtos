484ef02db3a20222dbce7a9d2f4b0cb2c760e3d1
miguelcarvalho-prog <miguel.carvalho@ba.estudante.senai.br>
2026-09-22 19:33:39 +0000

    cria estrutura do backend

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,31 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    const produto = {
-        id: 1,
-        nome: "Notebook",
-        preco: 3500,
-        estoque: 10,
-        categoria: "Eletrônicos"
-    };
+    const produtos = [
+        {
+            id: 1,
+            nome: "Notebook",
+            preco: 3500,
+            estoque: 10,
+            categoria: "Eletrônicos"
+        },
+        {
+            id: 2,
+            nome: "Celular",
+            preco: 1800,
+            estoque: 15,
+            categoria: "Eletrônicos"
+        },
+        {
+            id: 3,
+            nome: "Mochila",
+            preco: 120,
+            estoque: 20,
+            categoria: "Acessórios"
+        }
+    ];
 
-    res.json(produto);
+    res.json(produtos);
 });
 
 app.listen(3000, () => {
